76b67a42c914409ddfad0dc34517a840415bde38
nadeem danish <nadeemdanish.9188.1@gmail.com>
2025-06-24 22:20:24 +0500

    initialize dashboard screen layout

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -3,16 +3,45 @@ import 'package:flutter/material.dart';
 class DashboardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(40), // 👈 Round only this corner
+        )),
+
         title: Text(''),
+        toolbarHeight: screenHeight * .25, // 15% of screen width
+        backgroundColor: Colors.lightBlueAccent,
       ),
       body: Center(
-        child: Text(
-          'Welcome to the Dashboard!',
-          style: TextStyle(fontSize: 24),
-        ),
-      ),
+          child: Stack(
+        children: [
+          Container(
+            color: Colors.lightBlueAccent,
+            height: double.infinity,
+            width: double.infinity,
+          ),
+          Container(
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(90), // 👈 Round only this corner
+                ),
+              ),
+              height: double.infinity,
+              width: double.infinity,
+              child: Center(
+                child: Text(
+                  'Welcome to the Dashboard!',
+                  style: TextStyle(fontSize: 24),
+                ),
+              )),
+        ],
+      )),
     );
   }
 }
