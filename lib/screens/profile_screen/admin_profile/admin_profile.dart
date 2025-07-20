6744305e94db05e27f4b41af4e4369111b4ec67c
nadeem danish <nadeemdanish.9188.1@gmail.com>
2025-07-20 07:51:15 +0500

    Refactor AdminProfileScreen: update tab structure and improve layout for better user experience

--- lib/screens/profile_screen/admin_profile/admin_profile.dart
+++ lib/screens/profile_screen/admin_profile/admin_profile.dart
@@ -11,19 +11,36 @@ class _AdminProfileScreenState extends State<AdminProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Admin Profile'),
-            centerTitle: true,
-            bottom: const TabBar(
-                indicatorColor: Colors.lightBlueAccent,
-                indicatorWeight: 3,
-                labelColor: Colors.lightBlueAccent,
-                unselectedLabelColor: Colors.grey,
-                labelStyle: TextStyle(fontSize: 15),
-                tabs: [Text('meal'), Text('expense'), Text('profile')]),
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          backgroundColor: Colors.grey[100],
+          title: const Text('Admin Profile'),
+          centerTitle: true,
+          bottom: const TabBar(
+            indicatorColor: Colors.lightBlueAccent,
+            indicatorWeight: 3,
+            labelColor: Colors.lightBlueAccent,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(fontSize: 15),
+            tabs: [
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text('meal'),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text('dashboard'),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text('expense'),
+              )
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
