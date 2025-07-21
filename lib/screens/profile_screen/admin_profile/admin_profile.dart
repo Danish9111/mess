909e1247ce53642d895ee628e9e243db2ce3f2ab
nadeem danish <nadeemdanish.9188.1@gmail.com>
2025-07-21 21:37:22 +0500

    Refactor AdminProfileScreen: remove TimePicker24Hour and integrate MealTimeSetterScreen for meal time management

--- lib/screens/profile_screen/admin_profile/admin_profile.dart
+++ lib/screens/profile_screen/admin_profile/admin_profile.dart
@@ -51,167 +51,6 @@ class _AdminProfileScreenState extends State<AdminProfileScreen> {
   }
 }
 
-// class TimePicker24Hour extends StatefulWidget {
-//   const TimePicker24Hour({super.key});
-
-//   @override
-//   State<TimePicker24Hour> createState() => _TimePicker24HourState();
-// }
-
-// class _TimePicker24HourState extends State<TimePicker24Hour> {
-//   TimeOfDay? selectedTime;
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Colors.grey[100],
-//       body: Column(
-//         children: [
-//           Padding(
-//             padding: const EdgeInsets.only(top: 40),
-//             child: ElevatedButton(
-//               style: ElevatedButton.styleFrom(
-//                 padding:
-//                     const EdgeInsets.symmetric(horizontal: 32, vertical: 14),
-//                 shape: RoundedRectangleBorder(
-//                     borderRadius: BorderRadius.circular(12)),
-//               ),
-//               child: Text(
-//                 selectedTime != null
-//                     ? '${selectedTime!.hour.toString().padLeft(2, '0')}:${selectedTime!.minute.toString().padLeft(2, '0')}'
-//                     : 'Pick Time',
-//               ),
-//               onPressed: () async {
-//                 final TimeOfDay? time = await showTimePicker(
-//                   context: context,
-//                   initialTime: TimeOfDay.now(),
-//                   builder: (context, child) {
-//                     return MediaQuery(
-//                       data: MediaQuery.of(context)
-//                           .copyWith(alwaysUse24HourFormat: false),
-//                       child: Theme(
-//                         data: ThemeData.light().copyWith(
-//                           colorScheme: const ColorScheme.light(
-//                             primary: Colors.blueAccent,
-//                             onPrimary: Colors.white,
-//                             surface: Colors.white,
-//                             onSurface: Colors.black87,
-//                           ),
-//                           dialogBackgroundColor: Colors.white,
-//                           timePickerTheme: const TimePickerThemeData(
-//                             dayPeriodColor: Colors.lightBlueAccent,
-//                             dayPeriodTextColor: Colors.black,
-//                             hourMinuteColor: Colors.white,
-//                             hourMinuteTextColor: Colors.black,
-//                             dialHandColor: Colors.lightBlueAccent,
-//                             dayPeriodBorderSide: BorderSide(
-//                               color: Colors.lightBlueAccent,
-//                               width: 2,
-//                             ),
-//                           ),
-//                         ),
-//                         child: child!,
-//                       ),
-//                     );
-//                   },
-//                 );
-
-//                 if (time != null) {
-//                   setState(() => selectedTime = time);
-//                   debugPrint('Time picked: $time');
-//                 }
-//               },
-//             ),
-//           ),
-//           const SizedBox(height: 20),
-//           const Expanded(
-//               // 👈 THIS FIXES EVERYTHING
-//               child: MealTimeSetterScreen()),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
-class ExpandableContainerList extends StatefulWidget {
-  const ExpandableContainerList({super.key});
-
-  @override
-  _ExpandableContainerListState createState() =>
-      _ExpandableContainerListState();
-}
-
-class _ExpandableContainerListState extends State<ExpandableContainerList> {
-  int expandedIndex = -1;
-
-  @override
-  Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: ListView.builder(
-        shrinkWrap: true,
-        physics: const NeverScrollableScrollPhysics(),
-        itemCount: 7, // number of cousin containers
-        itemBuilder: (context, index) {
-          bool isExpanded = expandedIndex == index;
-          final days = [
-            'Sunday',
-            'Monday',
-            'Tuesday',
-            'Wednesday',
-            'Thursday',
-            'Friday',
-            'Saturday'
-          ];
-          return GestureDetector(
-            onTap: () {
-              setState(() {
-                expandedIndex = isExpanded ? -1 : index;
-              });
-            },
-            child: AnimatedContainer(
-                duration: const Duration(milliseconds: 300),
-                margin: const EdgeInsets.all(12),
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: isExpanded
-                      ? Colors.lightBlueAccent
-                      : Colors.blueAccent.shade200,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                height: isExpanded ? 200 : 80, // toggle height
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Center(
-                        child: Text(
-                          days[index],
-                          style: const TextStyle(
-                              color: Colors.white70, fontSize: 18),
-                        ),
-                      ),
-                      const SizedBox(height: 16),
-                      if (isExpanded) ...[
-                        const SizedBox(height: 16),
-                        const Text("More field 1",
-                            style: TextStyle(color: Colors.white70)),
-                        const Text("More field 2",
-                            style: TextStyle(color: Colors.white70)),
-                        // Add more widgets here
-                      ],
-                    ],
-                  ),
-                )),
-          );
-        },
-      ),
-    );
-  }
-}
-// import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
-// import 'package:firebase_firestore/firebase_firestore.dart';
-
 class MealTimeSetterScreen extends StatefulWidget {
   const MealTimeSetterScreen({super.key});
 
@@ -294,12 +133,9 @@ class _MealTimeSetterScreenState extends State<MealTimeSetterScreen> {
   Widget build(BuildContext context) {
     final isSmallScreen = MediaQuery.of(context).size.width < 600;
 
-    return SingleChildScrollView(
-        child: Container(
-      height: MediaQuery.of(context).size.height * 0.9,
+    return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text('Set Meal Times',
               style: Theme.of(context).textTheme.headlineSmall?.copyWith(
@@ -336,13 +172,13 @@ class _MealTimeSetterScreenState extends State<MealTimeSetterScreen> {
           ),
         ],
       ),
-    ));
+    );
   }
 
   Widget _buildMealCard(String meal, bool isSmallScreen) {
     return Card(
       color: Colors.white,
-      elevation: 2,
+      elevation: 0,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16),
       ),
@@ -406,7 +242,7 @@ class _MealTimeSetterScreenState extends State<MealTimeSetterScreen> {
           const SizedBox(width: 8),
           Text(
             '${type == 'start' ? 'Start' : 'End'}: ${formatTime(mealTimings[meal]![type]!)}',
-            style: const TextStyle(fontSize: 16),
+            style: const TextStyle(color: Colors.lightBlueAccent, fontSize: 16),
           ),
         ],
       ),
